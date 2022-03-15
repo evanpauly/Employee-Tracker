a03ea5183d9da8259eb3260d96af93990ba28013
evanpauly <94558036+evanpauly@users.noreply.github.com>
2022-03-14 21:54:21 -0500

    creating menu

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const inquirer = require('inquirer');
 const menu = require('node-menu');
+
+
+
+
+
+menu.addDelimiter('-', 33, 'Employee Tracker')
+    .addItem(
+        'View All Employees'
+    )
+    .addItem(
+        'Add Employee'
+    );
+
+
+
+menu.start();
