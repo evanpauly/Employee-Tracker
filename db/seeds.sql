85ff4ef51deb7cf64661244f16ef676952af80aa
evanpauly <94558036+evanpauly@users.noreply.github.com>
2022-03-16 10:00:11 -0500

    moving code to proper files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO department (name,id)
+VALUES
+  ('Human Resources', '1'),
+  ('Sales', '2'),
+  ('Legal', '3'),
+  ('Engineering', '4');
--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '',
+      database: 'employees'
+    }
+  );
 
 const menu = () => {
     return inquirer.prompt([
         {
             type: 'list',
             name: 'What would you like to do?',
-            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role']
-        }
+            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role', 'Quit']
+        },
+        //{
+        //    type: 'input',
+        //    name: 'Add Department',
+        //    message: 'What is the name of the department?'
+        //}
     ])
 }
+
+
 
 
 menu();
